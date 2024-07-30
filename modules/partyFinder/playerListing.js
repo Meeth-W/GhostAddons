90eaf9829897e13d4469cad77dc6939d591e3de3
Meeth <meeth2111@gmail.com>
2024-07-30 12:01:59 +0530

    ✔ Infrastructure for player listing.

---
+++ modules/partyFinder/playerListing.js
@@ -0,0 +1,8 @@
+import config from "../../config";
+
+export function listingCommand(type, ...args) {
+	if (!args) args = [];
+	args = args.filter(arg => arg !== null);
+
+	// TODO: Implementation
+}
